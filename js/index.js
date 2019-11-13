a0057aa35b95127dd090859f091a97d76bce0dbd
huy358 <huy358163@gmail.com>
2019-11-13 09:34:56 +0700

    complete all pages

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-var timerStart = Date.now();
+// var timerStart = Date.now();
 
-$(document).ready(function() {
-    console.log("Time until DOMready: ", Date.now()-timerStart);
-});
+// $(document).ready(function() {
+//     console.log("Time until DOMready: ", Date.now()-timerStart);
+// });
 
-$(window).load(function() {
-    console.log("Time until everything loaded: ", Date.now()-timerStart);
-});
+// $(window).load(function() {
+//     console.log("Time until everything loaded: ", Date.now()-timerStart);
+// });
